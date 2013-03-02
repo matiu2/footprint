d9ab4cd3e15652fdfe88266a1c6569c50645fb6e
Matthew Sherborne <msherborne@gmail.com>
2013-03-02 15:59:54 +1000

    Sorting out the directory layout. Gives a 'hello' site

--- site/App.hpp
+++ site/App.hpp
@@ -4,13 +4,16 @@
 
 #include <Wt/WApplication>
 #include <Wt/WEnvironment>
+#include <Wt/WText>
 
 namespace footprint {
 
     /// The Application object for the footprint security website
     class App : public Wt::WApplication {
     public:
-        App(const Wt::WEnvironment& env) : Wt::WApplication(env) {}
+        App(const Wt::WEnvironment& env) : Wt::WApplication(env) {
+            new Wt::WText("Hello", root());
+        };
     };
 
 }
