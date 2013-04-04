0d5ad57b85ed7cd141cd51358fea4790d6ccd5af
Matthew Sherborne <msherborne@gmail.com>
2013-04-05 07:14:48 +1000

    Starting to get a nicer SubDivide thing going

--- lib/wittyPlus/SubDivided.hpp
+++ lib/wittyPlus/SubDivided.hpp
@@ -13,116 +13,120 @@
 namespace wittyPlus {
 
 template<typename T>
-struct SubDivided {
+struct Part {
     typedef typename T::const_iterator const_iterator;
     typedef typename T::value_type value_type;
-    const T* original=nullptr;
-    value_type delimeter = value_type();
-    std::pair<const_iterator, const_iterator> range;
+    typedef std::pair<const_iterator, const_iterator> range_type;
+
+    range_type range;
+
+    Part(range_type range) : range(range) {}
+    Part(const_iterator a, const_iterator b) : range{a, b} {}
+    Part() : range{const_iterator(), const_iterator()} {}
+
     const_iterator begin() const { return range.first; }
     const_iterator end() const { return range.second; }
-    SubDivided(const T& original, value_type delim)
-        : original(&original), delimeter(delim),
-          range(get_next(original.begin())) {}
-    SubDivided() {}
-    template <typename T2>
-    bool compare(const T2* other, std::function<bool(value_type, T2)> pred) const {
-        /// Compares until *other is false. Useful for comparing to char*
-        auto first1 = begin();
-        auto last1 = end();
-        while ((first1 != last1) && (*other)) {
-            if (!pred(*(first1++), *(other++)))
-                return false;
-        }
-        return (first1 == end()) && ((bool)(*other) == false);
-    }
-    template <typename T2>
-    bool compare(const T2& other, std::function<bool(value_type, typename T2::value_type)> pred) const {
-        /// Compares against another container
-        auto first1 = begin();
-        auto last1 = end();
-        auto first2 = other.begin();
-        auto last2 = other.end();
-        if (begin() == end())
-            return other.begin() == other.end();
-        while ((first1 != last1) && (first2 != last2)) {
-            if (!pred(*(first1++), *(first2++)))
-                return false;
-        }
-        return true;
-    }
-    std::pair<const_iterator, const_iterator> get_next(const_iterator start=const_iterator()) const {
-        if (original != nullptr) {
-            auto next = start == const_iterator() ? end() : start,
-                 last = next;
-            do {
-                next = std::find(last, original->end(), delimeter);
-                // Only push non-empty references
-                if (last != next)
-                    return make_pair(last, next);
-                last = next+1;
-            } while (next != original->end());
-        }
-        return make_pair(const_iterator(), const_iterator());
-    }
-    SubDivided<T>& operator++() {
-        range = get_next();
-        return *this;
-    }
-    SubDivided<T> operator++(int) {
-        SubDivided<T> result(*this);
-        ++(*this);
-        return result;
-    }
+
     operator bool() {
         return range != make_pair(const_iterator(), const_iterator());
     }
-    bool operator ==(const SubDivided<T>& other) {
-        return compare(other, [](value_type a, value_type b){ return a == b; });
+
+    template<typename T2>
+    bool operator ==(const T2* other) const {
+        /// Compares against pointer types, useful for pchar
+        const T2* last = other;
+        while (*last++) {} // Find the end (eg. '\0' for char*)
+        auto result = std::mismatch(begin(), end(), other);
+        return result == make_pair(end(), last-1);
     }
-    bool operator !=(const SubDivided<T>& other) { return !(*this == other); }
-    template <typename T2>
-    bool operator ==(const T2& other) const { return compare(other, [](value_type a, typename T2::value_type b){ return a == b; }); }
-    template <typename T2>
-    bool operator ==(const T2* other) const { return compare<T2>(other, [](value_type a, T2 b){ return a == b; }); }
+    template<typename T2>
+    bool operator ==(const T2& other) const {
+        auto result = std::mismatch(begin(), end(), other.begin());
+        return result == make_pair(end(), other.end());
+    }
+    template<typename T2>
+    bool operator !=(const T& other) const { return !((*this) == other); }
 
-    template <typename T2>
-    bool operator !=(const T2& other) const { return !((*this) == other); }
-    template <typename T2>
-    bool operator !=(const T2* other) const { return !compare<T2>(other, [](value_type a, T2 b){ return a == b; }); }
+    bool operator !=(const T& other) const { return !((*this) == other); }
+    bool operator !=(const T* other) const { return !((*this) == other); }
 
-    template <typename T2>
-    bool operator <(const T2& other) const { return compare(other, [](value_type a, typename T2::value_type b){ return a < b; }); }
-    template <typename T2>
-    bool operator <(const T2* other) const { return compare<T2>(other, [](value_type a, T2 b){ return a < b; }); }
+    template<typename T2>
+    bool operator <(const T2& other) const {
+        return std::lexicographical_compare(begin(), end(), other.begin(), other.end());
+    }
+    bool operator <(const T* other) const {
+        T* last = other;
+        while (*last++) {} // Find the end (eg. '\0' for char*)
+        return std::lexicographical_compare(begin(), end(), other, last-1, [](T a, T b){return a <= b;});
+    }
 
-    template <typename T2>
-    bool operator >(const T2& other) const { return compare(other, [](value_type a, typename T2::value_type b){ return a > b; }); }
-    template <typename T2>
-    bool operator >(const T2* other) const { return compare<T2>(other, [](value_type a, T2 b){ return a > b; }); }
-    
-    template <typename T2>
-    bool operator <=(const T2& other) const { return compare(other, [](value_type a, typename T2::value_type b){ return a <= b; }); }
-    template <typename T2>
-    bool operator <=(const T2* other) const { return compare<T2>(other, [](value_type a, T2 b){ return a <= b; }); }
+    /*
+    bool operator >(const T& other) const { return !((*this < other)); }
+    bool operator >(const T* other) const { return !((*this < other)); }
 
-    template <typename T2>
-    bool operator >=(const T2& other) const { return compare(other, [](value_type a, typename T2::value_type b){ return a >= b; }); }
-    template <typename T2>
-    bool operator >=(const T2* other) const { return compare<T2>(other, [](value_type a, T2 b){ return a >= b; }); }
+    bool operator <=(const T& other) const { return !((*this > other)); }
+    bool operator <=(const T* other) const { return !((*this > other)); }
+
+    bool operator >=(const T& other) const { return compare(other, [](value_type a, typename value_type b){ return a >= b; }); }
+    bool operator >=(const T* other) const { return compare(other, [](value_type a, T b){ return a >= b; }); }
+    */
 };
 
-bool operator !=(const char* a, const SubDivided<std::string>& b) { return !(b == a); }
-bool operator !=(std::string a, const SubDivided<std::string>& b) { return !(b == a); }
+bool operator !=(const char* a, const Part<std::string>& b) { return !(b == a); }
+bool operator !=(const std::string& a, const Part<std::string>& b) { return !(b == a); }
+template <typename T>
+bool operator !=(const Part<T>& a, const Part<T>& b) { return !(b == a); }
 
 template <typename T>
-std::ostream& operator <<(std::ostream& out, const SubDivided<T>& s) {
+struct DelimitedPart : public Part<T> {
+    typedef typename Part<T>::const_iterator const_iterator;
+    typedef typename Part<T>::value_type value_type;
+    typedef typename Part<T>::range_type range_type;
+
+    value_type delimeter;
+    range_type maxRange;
+
+    DelimitedPart(range_type aMaxRange, value_type delim)
+        : Part<T>(aMaxRange), maxRange(aMaxRange), delimeter(delim)
+    {
+        Part<T>::range = get_next(aMaxRange.first);
+    }
+
+    range_type get_next(const_iterator start=const_iterator()) const {
+        auto next = start == const_iterator() ? Part<T>::end() : start,
+             last = next;
+        do {
+            next = std::find(last, maxRange.second, delimeter);
+            // Only push non-empty references
+            if (last != next)
+                return make_pair(last, next);
+            last = next+1;
+        } while (next != maxRange.second);
+        return make_pair(const_iterator(), const_iterator());
+    }
+    DelimitedPart<T>& operator++() {
+        Part<T>::range = get_next();
+        return *this;
+    }
+    DelimitedPart<T> operator++(int) {
+        DelimitedPart<T> result(*this);
+        ++(*this);
+        return result;
+    }
+};
+
+template <typename T>
+std::ostream& operator <<(std::ostream& out, const Part<T>& s) {
     auto i = s.begin();
     while (i != s.end())
         out << *(i++);
     return out;
 }
 
-typedef SubDivided<std::string> SubDividedString;
+template <typename T>
+DelimitedPart<T> split(const T& whole, typename T::value_type delimeter) {
+    return DelimitedPart<T>(make_pair(whole.begin(), whole.end()), delimeter);
+}
+
 
 }
--- lib/wittyPlus/tests/testSubDivided.cpp
+++ lib/wittyPlus/tests/testSubDivided.cpp
@@ -4,22 +4,37 @@
 #include "../SubDivided.hpp"
 #include "Testing.hpp"
 
+#include <vector>
+#include <algorithm>
+
 int main(int, char**) {
     std::string path = "/path/to/somewhere/";
 
-    auto part = wittyPlus::SubDivided<std::string>(path, '/');
+    auto part = wittyPlus::split(path, '/');
 
     assertEqual("path", part);
     ++part;
     assertEqual("to", part++);
     assertEqual("somewhere", part++);
-    assertEqual(wittyPlus::SubDivided<std::string>(), part);
+    assertEqual(wittyPlus::Part<std::string>(), part);
     assertEqual("", part);
 
-    part = wittyPlus::SubDivided<std::string>(path, '/');
+    // Does it work with strings ?
+    part = wittyPlus::split(path, '/');
 
     assertEqual(std::string("path"), part++);
     assertEqual(std::string("to"), part++);
     assertEqual(std::string("somewhere"), part++);
+
+    // Can you sort them ?
+    std::vector<decltype(part)> parts;
+    part = wittyPlus::split(path, '/');
+    while (part)
+        parts.push_back(part++);
+    std::sort(parts.begin(), parts.end());
+
+    assertEqual("path", parts[0]);
+    assertEqual("somewhere", parts[1]);
+    assertEqual("to", parts[2]);
 
 }
